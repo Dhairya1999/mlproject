bec3852cf64b4cff4efd38288fdcab5d433aa28d
sohamsavalapurkar <soham1999@gmail.com>
2022-04-23 11:16:06 -0500

    dataset creation added in preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,9 +3,13 @@ import numpy as np
 class preprocess_data:
     def __init__(self) -> None:
         self.data = pd.read_csv('Data/PET_PRI_GND_DCUS_NUS_W.csv')
-    def preproces(self):
+    def preproces(self, fuel_grade):
         #Since the output for this is False, there is no null data
         print(self.data.isnull().sum().any())
-
+        X, Y = [], []
+        for i in range(len(self.data[fuel_grade])-1):
+            X.append(self.data[fuel_grade][i])
+            Y.append(self.data[fuel_grade][i+1])
+        return np.array(X), np.array(Y)
 p = preprocess_data()
-p.preproces()
+print(p.preproces(fuel_grade='A2'))
