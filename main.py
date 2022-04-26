29be5c01db9bfc7cc341705598fe3091505c8c70
sohamsavalapurkar <soham1999@gmail.com>
2022-04-25 22:22:23 -0500

    main file updated

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from model import Model
 from eda import exploratory_data_analysis
 if __name__ == '__main__':
 
-    #data = exploratory_data_analysis()
-    #print(data.eda())
+    data = exploratory_data_analysis()
+    print(data.eda())
     model = Model()
     model.model()
     print(model.test_predict())
